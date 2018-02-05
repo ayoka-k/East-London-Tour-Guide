b2b8c245ca36ba62dabff60bdb37f77a0a112232
Alena Kosheleva <alena@georges-mbp.mynet>
2018-02-05 18:09:15 +0000

    Change activity_main layout to display four fragments. Add CategoryAdapter class to display fragments on the screen correctly.

--- app/src/main/java/com/example/alena/eastlondonguide/CategoryAdapter.java
+++ app/src/main/java/com/example/alena/eastlondonguide/CategoryAdapter.java
@@ -1,8 +1,65 @@
 package com.example.alena.eastlondonguide;
 
-/**
- * Created by alena on 2/5/18.
- */
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 
-public class CategoryAdapter {
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    /**
+     * Context of the app
+     */
+    private Context mContext;
+
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     *
+     * @param fragmentManager is the fragment manager that will keep each fragment's state in the adapter
+     *                        across swipes.
+     */
+    public CategoryAdapter(Context context, FragmentManager fragmentManager) {
+        super(fragmentManager);
+        mContext = context;
+    }
+
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new TowerFragment();
+        } else if (position == 1) {
+            return new RiversideFragment();
+        } else if (position == 2) {
+            return new IsleOfDogsFragment();
+        } else {
+            return new EastEndFragment();
+        }
+    }
+
+    /**
+     * Return the total number of pages.
+     */
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    /**
+     * Return the {@link String} that should be displayed for the given page number.
+     */
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_tower);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_riverside);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_isleOfDogs);
+        } else {
+            return mContext.getString(R.string.category_eastEnd);
+        }
+    }
 }
--- app/src/main/java/com/example/alena/eastlondonguide/EastEndFragment.java
+++ app/src/main/java/com/example/alena/eastlondonguide/EastEndFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class EastEndFragment extends Fragment {
 
@@ -15,13 +14,10 @@ public class EastEndFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_main, container, false);
     }
-
 }
